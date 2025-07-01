2d569f50c9f96dd0b8320d785147c6def65c330e
Boudnai <156792201+Boudnai@users.noreply.github.com>
2025-07-01 11:58:21 +0200

    Update extract_numbers.py

--- extract_numbers.py
+++ extract_numbers.py
@@ -1,24 +1,20 @@
 import re
 
-# Lire le contenu du fichier .ics
-file_path = "Efoil_Cogolin_qqquf6itrgkuqg03u7epud71b8@group.calendar.google.com.ics"  # Remplacez par le chemin de votre fichier
+file_path = "Path_of_your_google_calendar_file.ics"  
 with open(file_path, 'r', encoding='utf-8') as file:
     content = file.read()
 
-# Rechercher les numéros de téléphone
 pattern = r'\b(?:\+33\s?[1-9](?:\s?\d{2}){4}|0[67](?:\s?\d{2}){4})\b'
 phone_numbers = re.findall(pattern, content)
 
-# Afficher les numéros trouvés
 i= 0
 for number in phone_numbers:
     print(number)
     print (i)
     i = i + 1
 
-# Si vous voulez sauvegarder les numéros dans un fichier :
 output_path = "phone_numbers.txt"
 with open(output_path, 'w', encoding='utf-8') as output_file:
     output_file.write('\n'.join(phone_numbers))
-print(f"Les numéros de téléphone ont été sauvegardés dans {output_path}")
-print ("\n il y a ", i, " numéros de téléphone")
+print(f"Phone numbers are save at {output_path}")
+print ("\n There is ", i, " phone numbers")
